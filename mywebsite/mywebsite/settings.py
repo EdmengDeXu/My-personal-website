653d70c40ea39e1a480b0ff173e9c652eef6a322
宇宙 <2677267624@qq.com>
2018-05-20 14:07:52 +0800

    create media

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/' # 配置媒体网址
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 配置媒体根目录
--- mywebsite/showme/models.py
+++ mywebsite/showme/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class ShowMe(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default='add a description',max_length=100)
+    image = models.ImageField(default='default.png',upload_to='images/')
+    title = models.CharField(default='title',max_length=10)
+
+    def __str__(self):
+        return self.title
