9eb566c2bd664a888934451f525ac9242e8cb3a7
宇宙 <2677267624@qq.com>
2018-05-20 15:32:47 +0800

    add blog app

---
+++ mywebsite/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    date = models.DateField()
+    image = models.ImageField(default='default.jpeg',upload_to='images/')
+    title = models.CharField(default='title',max_length=50)
+    text = models.TextField(default='your text')
+
+    def __str__(self):
+        return self.title
